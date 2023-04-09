c467e2db471e55a0a7db9e2d8fca86b9d155956d
sysun0813 <sysun0813@naver.com>
2023-04-09 15:40:28 +0900

    FadeInOut 제작 및 텍스트 효과 추가
    a -> FadeIn
    b -> FadeOut
    space -> text anim

---
+++ GameMediaProject/Assets/swim/Scripts/FadeInOut.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeInOut : MonoBehaviour
+{
+
+    public float fadeTime = 1.0f;
+
+    private Material material;
+
+    private void Awake()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        material = renderer.material;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            StartCoroutine(FadeIn());
+        }
+        if(Input.GetKeyDown(KeyCode.B))
+        {
+            StartCoroutine(FadeOut());
+        }
+    }
+
+    IEnumerator FadeIn()
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / fadeTime;
+            float alpha = Mathf.Lerp(0f, 1f, t);
+            material.color = new Color(material.color.r, material.color.g, material.color.b, alpha);
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeOut()
+    {
+        float t = 0f;
+
+        while (t < 1f)
+        {
+            t += Time.deltaTime / fadeTime;
+            float alpha = Mathf.Lerp(1f, 0f, t);
+            material.color = new Color(material.color.r, material.color.g, material.color.b, alpha);
+            yield return null;
+        }
+    }
+}
---
+++ GameMediaProject/Assets/swim/Scripts/TextControll.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextControll : MonoBehaviour
+{
+
+    public Text text;
+
+    public string[] sentences = { "test 1", "test 2", "test 3" };
+    private int currentIndex = 0;
+
+    public float typeSpeed = 0.1f;
+
+    private IEnumerator TypeSentence(string sentence)
+    {
+        text.text = "";
+        foreach(char letter in sentence.ToCharArray())
+        {
+            text.text += letter;
+            yield return new WaitForSeconds(typeSpeed);
+        }
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            StartTyping();
+        }
+    }
+
+    public void StartTyping()
+    {
+        if(currentIndex < sentences.Length)
+        {
+            string sentence = sentences[currentIndex];
+            currentIndex++;
+
+            StartCoroutine(TypeSentence(sentence));
+        }
+    }
+}
